8a9d7c4686024b5e44388f199c627fe0f0c53386
utkarsh <utkarshrai003@gmail.com>
2017-05-18 12:56:37 +0530

    Adds Body Parser in server.js

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express');
 var database = require('./config/connection.js');
 var app = express();
 
+// Body Parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(require('./app/controllers'));
+
 app.listen(8081, function () {
   console.log('Server started at port: 8081');
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
 });
