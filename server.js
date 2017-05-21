e93d1c5b2fea3544ffdb94984e4feff3ffd95887
utkarsh <utkarshrai003@gmail.com>
2017-05-21 17:18:19 +0530

    Adds express-session to the server.js file

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 
 var express = require('express');
-var database = require('./config/connection.js');
 var app = express();
+
+// Database
+var database = require('./config/connection.js');
 
 // Body Parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// Seesions
+var session = require('express-session')
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.use(express.static('public/images'));
 
-app.use(require('./app/controllers'));
+app.use(require('./app/controllers/users.js'));
 
 app.listen(8081, function () {
   console.log('Server started at port: 8081');
 });
+
+module.exports = router;
