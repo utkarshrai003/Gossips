a926db7e0dcb7f9d4067396c44ce1228c16c46e4
utkarsh <utkarshrai003@gmail.com>
2017-05-21 17:17:50 +0530

    Commented the code index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,21 @@
-
-var express = require('express');
-var router = express.Router();
-
-router.use('/', require('./users'));
-
-module.exports = router;
+// 
+// var express = require('express');
+// var router = express.Router();
+//
+// router.use('/', require('./users'));
+//
+// function isUserAuthenticated(req,res,next){
+// //you can check anything heref like req.url ,session ect.
+//
+// //so let's check if user session exists
+// // if(req.session.user){
+// // //user logged in
+// // return next();
+// // }
+// // //user not authenticated redirect them to login page or anywhere you want
+// // res.redirect("/welcome");
+// }
+//
+// router.use(isUserAuthenticated);
+//
+// module.exports = router;
