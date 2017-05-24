f10b510043acb615f82895ddab2d34a8d3c3896f
utkarsh <utkarshrai003@gmail.com>
2017-05-24 16:40:19 +0530

    Replaces sender_id and receiver_id to be of type string

--- app/models/friend_request.js
+++ app/models/friend_request.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var friendRequestSchema = new Schema({
-  sender_id: Number,
-  receiver_id: Number,
-  status: { type: String, enum: ['pending', 'accepted'. 'declined'] }
+  sender_id: { type: String, required: true },
+  receiver_id: { type: String, required: true },
+  status: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending'}
 });
 
-var FriendRequest = mongoose.model('FriendRequest', FriendRequestSchema);
+var FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
 
 module.exports = FriendRequest;
