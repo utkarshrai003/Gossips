1c986b831e052d3a9aebefeb491851fb8d2492ea
utkarsh <utkarshrai003@gmail.com>
2017-06-02 17:41:19 +0530

    Refactored endpoint to accept friend requests for the user

--- app/routes/friend_requests.js
+++ app/routes/friend_requests.js
@@ -11,7 +11,7 @@ router.post('/send_request', isUserAuthenticated, function(req, res) {
   var receiver_id = req.body.to_user;
 
   var promise = FriendRequest.findOne({
-    $or: [{sender_id: sender_id, receiver_id: receiver_id}, {receiver_id: sender_id, sender_id: receiver_id}]},
+    $or: [{sender: sender_id, receiver: receiver_id}, {receiver: sender_id, sender: receiver_id}]},
     {}, {sort: {'created_at': -1}}).exec();
 
   // If request is not already there, or is not accepted and pending
@@ -20,8 +20,8 @@ router.post('/send_request', isUserAuthenticated, function(req, res) {
     console.log("checkpoint 1");
     if( !friend_request || ( friend_request && !(friend_request.status=='accepted' || friend_request.status=='pending')) ) {
       var request = new FriendRequest({
-        sender_id: sender_id,
-        receiver_id: receiver_id
+        sender: sender_id,
+        receiver: receiver_id
       });
       return request.save();
     }
@@ -32,20 +32,20 @@ router.post('/send_request', isUserAuthenticated, function(req, res) {
   // Get all the requests to be sent to the receiver via sockets
   .then(function(friend_request) {
     console.log("checkpoint 2" + friend_request);
-    return FriendRequest.find({receiver_id: receiver_id, status: 'pending'}).exec();
+    return FriendRequest.find({receiver: receiver_id, status: 'pending'}).exec();
   })
   // Get the user info from requests and prepae the object to be sent
   .then(function(requests) {
     console.log("checkpoint 3" + requests);
     var sender_ids = _.map(requests, function(obj) {
-      return obj.sender_id;
+      return obj.sender;
     });
     return User.find({_id: {$in: sender_ids}}).exec();
   })
   // Publish the requests and redirect
   .then(function(requests) {
     console.log("checkpoint 4" + requests);
-    req.app.io.emit(receiver_id, {type: 'notification', sub_type: 'friend_requests', data: {from: requests}});
+    // req.app.io.emit(receiver_id, {type: 'notification', sub_type: 'friend_requests', data: {from: requests}});
     res.redirect('/profile');
   })
   .catch(function(err) {
@@ -54,95 +54,88 @@ router.post('/send_request', isUserAuthenticated, function(req, res) {
 });
 
 router.get('/test', function(req, res) {
-  // FriendRequest.remove({}, function(err, record) {
-  //
-  // });
-  // res.send({data: "removed"});
-  User.find({}, function(err, records) {
-    if(err) {
-      res.send({status: 400, error: "My error"});
-    }
-    else {
-      res.send({status: 200, records: records});
-    }
+  FriendRequest.remove({}, function(err, record) {
+
   });
+  res.send({data: "removed"});
+  // User.find({}).populate("friends.user").exec(function(err, records) {
+  //   if(err) {
+  //     res.send({status: 400, error: err});
+  //   }
+  //   else {
+  //     res.send({status: 200, records: records});
+  //   }
+  // });
 });
 
 
 // CAN BE REFACTORED I THINK
 // Endpoint to return the users who sent friend requests to the currently logged in user
-// router.get('/friend_requests', isUserAuthenticated, function(req, res) {
-//   var promise = FriendRequest.find({receiver_id: req.session.user._id, status: 'pending'}).exec();
-//
-//   promise.then(function(requests) {
-//     var data = [];
-//     _.each(requests, function(request) {
-//       var sender_id = request.sender_id;
-//       User.findOneAsync({_id: sender_id}).exec(function(err, record) {
-//         data.push({
-//           request: request,
-//           sender: user
-//         });
-//       });
-//       console.log("Data is : " + data);
-//     });
-//     res.send({status: 200, senders: data});
-//   })
-//   .catch(function(err) {
-//     res.send({ status: 400, error: err });
-//   });
-
-
-  // promise.then(function(requests) {
-  //   var sender_ids = _.map(requests, function(obj) {
-  //     return obj.sender_id;
-  //   });
-  //   console.log("Sender ids =>  " + sender_ids);
-  //   return User.find({_id: {$in: sender_ids}}).exec();
-  // })
-  // .then(function(request_senders) {
-  //   console.log("Request senders => " + request_senders);
-  //   res.send({status: 200, senders: request_senders});
-  // })
-  // .catch(function(err) {
-  //   res.send({ status: 400, error: err });
-  // });
-// });
-
-// {
-//   request_id: ,
-//   sender_info:
-// }
+router.get('/friend_requests', isUserAuthenticated, function(req, res) {
+  var promise = FriendRequest.find({receiver: req.session.user._id, status: 'pending'}).populate('sender', '_id name username').exec();
+  promise.then(function(requests_with_sender) {
+    res.send({status: 200, data: requests_with_sender});
+  })
+  .catch(function(err) {
+    res.send({status: 400, error: err});
+  });
+});
 
 // request id is required
-// router.post('/accept_request', isUserAuthenticated, function(req, res) {
-//   var current_user_id = req.session.user._id;
-//   var friend_user_id = req.body.friend_id;
-//   var picked;
-//
-//   // For friend 1
-//   User.findOne({_id: current_user_id}).exec(function(err, record) {
-//     picked = _.pick(record, "_id", "name", "username");
-//     User.update({_id: current_user_id}, {$push: {friends: picked}}).exec(function(err, record) {
-//       // if(err) {
-//       //   res.send({ststus: 400, error: err});
-//       // }
-//     });
-//   });
-//
-//   // For friend 1
-//   User.findOne({_id: friend_user_id}).exec(function(err, record) {
-//     picked = _.pick(record, "_id", "name", "username");
-//     User.update({_id: friend_user_id}, {$push: {friends: picked }}).exec(function(err, record) {
-//       console.log('checkpoint 1');
-//       // if(err) {
-//       //   res.send({status: 400, error: err});
-//       // }
-//     });
-//   });
-//
-//   res.send({status: 200, data: "done"});
-// });
+router.post('/accept_request', isUserAuthenticated, function(req, res) {
+  var request_id = req.body.request_id;
+
+  promise  = FriendRequest.findOneAndUpdate({_id: request_id}, {status: 'accepted'}).exec();
+
+  // adding friend to receiver
+  promise.then(function(request) {
+    const sender = User.findOne({_id: request.sender}).exec();
+    const receiver = User.findOne({_id: request.receiver}).exec();
+    return Promise.all([sender, receiver]);
+  })
+  .then(resolves => {
+    const sender = resolves[0];
+    const receiver = resolves[1];
+    sender.friends.push(receiver._id);
+    receiver.friends.push(sender._id);
+
+    return Promise.all([ sender.save(), receiver.save() ]);
+  })
+  .then(resolves => {
+    res.send({status: 200, data: "done"});
+  })
+  .catch((err) => {
+    console.error(err);
+    res.send({status: 400, error: err});
+  });
+
+
+
+  //   , function(err, res) {
+  //     if(err) {
+  //       console.log("GOT ERROR" + err);
+  //     }
+  //     User.findOne({_id: request.receiver}, function(err, res2) {
+  //       console.log("Res 2 ", res2);
+  //       res.friends.push(res2._id);
+  //       res.save().catch(console.log)
+  //       // console.log(res.save());
+  //       console.log("User is " + JSON.stringify(res.friends));
+  //     });
+  //     // res.friends.push(res);
+  //     // console.log(res.friends);
+  //     // res.save();
+  //     // console.log(res.save());
+  //   });
+  // })
+  // .then(function(user) {
+  //   console.log("User :" + user);
+  //   res.send({status: 200, data: "done"});
+  // })
+  // .catch(function(err) {
+  //   res.send({status: 400, error: err})
+  // })
+});
 
 
 module.exports = router;
